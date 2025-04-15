5af2dac2d5f07902d2f9b57e71b23debfc597ab9
vitorbinhotti <vitor_binhotti@estudante.sesisenai.org.br>
2025-04-15 09:25:46 -0300

    update - alteracoes

--- script.js
+++ script.js
@@ -1,23 +1,77 @@
 function buscaCep(cep) {
-    fetch('https://viacep.com.br/ws/'+cep+'/json/')
-    .then(response => {
-        if(!response.ok){
-            console.log("ERRO DE CONEXÃO")
-        }
+    fetch('https://viacep.com.br/ws/' + cep + '/json/')
+        .then(response => {
+            if (!response.ok) {
+                console.log("ERRO DE CONEXÃO")
+            }
 
-    return response.json()
-    })
-    .then(data => {
-        console.log(data)
-        endereco.value = data.logradouro
-        bairro.value = data.bairro
-        cidade.value = data.localidade
-        estado.value = data.uf
-    })
-    .catch(error => {
-        console.log("Erro: ", error)
-    })
+            return response.json()
+        })
+        .then(data => {
+            document.querySelector("#endereco").value = data.logradouro || "";
+            document.querySelector("#bairro").value = data.bairro || "";
+            document.querySelector("#cidade").value = data.localidade || "";
+            document.querySelector("#estado").value = data.uf || "";
+          })
+        .catch(error => {
+            console.log("Erro: ", error)
+        })
 }
+
+const form = document.querySelector("#form");
+const nameInput = document.querySelector("#name");
+const emailInput = document.querySelector("#email");
+const passwordInput = document.querySelector("#password");
+const jobSelect = document.querySelector("#job");
+const messageTextarea = document.querySelector("#message");
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    if (nameInput.value === "") {
+        alert("Por favor, preencha o seu nome");
+        return;
+    }
+
+    if (emailInput.value === "" || !isEmailValid(emailInput.value)) {
+        alert("Por favor, preencha o seu email");
+        return;
+    }
+
+    if (!validatePassword(passwordInput.value)) {
+        alert("A senha precisa ser no mínimo 8 digitos");
+        return;
+    }
+
+    if(messageTextarea.value === "") {
+        alert("Por favor, escreva uma mensagem");
+        return;
+    }
+
+    form.submit();
+});
+
+function isEmailValid(email) {
+
+    const emailRegex = new RegExp(
+        /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]{2,}$/
+    );
+
+    if (emailRegex.test(email)) {
+        return true;
+    }
+
+    return false;
+}
+
+function validatePassword(password, minDigits) {
+    if (password.lenght >= minDigits) {
+        return true
+    }
+
+    return false
+}
+
 
 function confereSenha() {
     const senha = document.querySelector('input[name=senha]');
@@ -31,5 +85,5 @@ function confereSenha() {
 }
 
 function senhaOK() {
-    alert ("Conectado com sucesso!")
+    alert("Conectado com sucesso!")
 }
